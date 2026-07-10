fe991602b5ad31a62ea17943a78831c63ffdcf2d
ronnyareu22 <ronnyareu22@gmail.com>
2026-07-09 22:00:27 -0500

    feat(ui): agregar acceso al grafo en el menu y rutas

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+from .views import git_history_view, git_history_api
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("historial/", git_history_view, name="historial"),
+    path("api/git-graph/", git_history_api, name="git_graph_api"),
     path("estudiantes/", include("estudiantes.urls")),
     path("cursos/", include("cursos.urls")),
     path("reportes/", include("reportes.urls")),
